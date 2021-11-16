2a50f51d1bb26ce15e7415a2a98ab4af788d0a2d
chemilk02 <79562050+chemilk02@users.noreply.github.com>
2021-11-16 09:13:33 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,55 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    a = a + 1
+    led.plot(a, b)
+    if (a > 4) {
+        basic.clearScreen()
+        a = 0
+        led.plot(a, b)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    b = b + 1
+    led.plot(a, b)
+    if (b > 4) {
+        basic.clearScreen()
+        b = 0
+        led.plot(a, b)
+    }
+})
+let b = 0
+let a = 0
+a = 0
+b = 0
+while (b < 5) {
+    while (a < 5) {
+        led.plot(a, b)
+        a = a + 1
+        basic.pause(100)
+    }
+    b = b + 1
+    a = 0
+    basic.pause(100)
+}
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+}
+a = 0
+b = 0
+led.plot(a, b)
